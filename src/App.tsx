5a6a231201ff2325755081d529ca98cfa2366876
Joshua Richardson <josh@cursedtale.com>
2024-09-20 11:47:35 -0700

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-function App() {
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
+
+export default function App() {
   return (
-    <div className='min-h-screen bg-neutral-950 text-white'>Hello world</div>
+    <div className='min-h-screen bg-neutral-950 text-white grid grid-cols-[1fr,auto]'>
+      <ResizablePanelGroup direction='horizontal'>
+        <ResizablePanel defaultSize={50}>
+          <div></div>
+        </ResizablePanel>
+
+        <ResizableHandle />
+
+        <ResizablePanel defaultSize={50}>
+          <div></div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
   )
 }
 
-export default App
+function getRandomTree(n = 300, reverse = false) {
+  return {
+    nodes: [...Array(n).keys()].map((i) => ({ id: i })),
+    links: [...Array(n).keys()]
+      .filter((id) => id)
+      .map((id) => ({
+        [reverse ? 'target' : 'source']: id,
+        [reverse ? 'source' : 'target']: Math.round(Math.random() * (id - 1)),
+      })),
+  }
+}
